95c28dfa929e74e201050ddd32120912cc319e27
Raymond An <raymondan100@gmail.com>
2021-01-25 09:52:54 -0800

    Finish Lab 1 Part 2

--- Lab-1/CalculatorLibrary/CalculatorLibrary.cs
+++ Lab-1/CalculatorLibrary/CalculatorLibrary.cs
@@ -2,7 +2,7 @@
 
 namespace CalculatorLibrary
 {
-    public class Class1
-    {
+    public class Class1 {
+
     }
 }
--- Lab-1/Lab-1/Program.cs
+++ Lab-1/Lab-1/Program.cs
@@ -1,37 +1,74 @@
 ﻿using System;
+using System.IO;
+using System.Diagnostics;
+using Newtonsoft.Json;
+using CalculatorLibrary;
 
-namespace Lab_1
+namespace CalculatorProgram
 {
-    class Calculator
+    public class Calculator
     {
-        public static double DoOperation(double num1, double num2, string op)
+        JsonWriter writer;
+
+        public Calculator()
+        {
+            StreamWriter logFile = File.CreateText("calculatorlog.json");
+            logFile.AutoFlush = true;
+            writer = new JsonTextWriter(logFile);
+            writer.Formatting = Formatting.Indented;
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operations");
+            writer.WriteStartArray();
+        }
+
+        public double DoOperation(double num1, double num2, string op)
         {
             double result = double.NaN; // Default value is "not-a-number" which we use if an operation, such as division, could result in an error.
-
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operand1");
+            writer.WriteValue(num1);
+            writer.WritePropertyName("Operand2");
+            writer.WriteValue(num2);
+            writer.WritePropertyName("Operation");
             // Use a switch statement to do the math.
             switch (op)
             {
                 case "a":
                     result = num1 + num2;
+                    writer.WriteValue("Add");
                     break;
                 case "s":
                     result = num1 - num2;
+                    writer.WriteValue("Subtract");
                     break;
                 case "m":
                     result = num1 * num2;
+                    writer.WriteValue("Multiply");
                     break;
                 case "d":
                     // Ask the user to enter a non-zero divisor.
                     if (num2 != 0)
                     {
                         result = num1 / num2;
+                        writer.WriteValue("Divide");
                     }
                     break;
                 // Return text for an incorrect option entry.
                 default:
                     break;
             }
+            writer.WritePropertyName("Result");
+            writer.WriteValue(result);
+            writer.WriteEndObject();
+
             return result;
+        }
+
+        public void Finish()
+        {
+            writer.WriteEndArray();
+            writer.WriteEndObject();
+            writer.Close();
         }
     }
 
@@ -43,7 +80,8 @@ namespace Lab_1
             // Display title as the C# console calculator app.
             Console.WriteLine("Console Calculator in C#\r");
             Console.WriteLine("------------------------\n");
-
+            Calculator calculator = new Calculator();
+    
             while (!endApp)
             {
                 // Declare variables and set to empty.
@@ -85,7 +123,7 @@ namespace Lab_1
 
                 try
                 {
-                    result = Calculator.DoOperation(cleanNum1, cleanNum2, op);
+                    result = calculator.DoOperation(cleanNum1, cleanNum2, op);
                     if (double.IsNaN(result))
                     {
                         Console.WriteLine("This operation will result in a mathematical error.\n");
@@ -105,7 +143,12 @@ namespace Lab_1
 
                 Console.WriteLine("\n"); // Friendly linespacing.
             }
+            // And call to close the JSON writer before return
+            calculator.Finish();
             return;
         }
     }
 }
+
+
+
