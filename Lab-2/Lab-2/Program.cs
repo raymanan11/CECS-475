e6a57367e70a58719d0aa944f84365cec296c50a
Raymond An <raymondan100@gmail.com>
2021-01-27 22:00:45 -0800

    Default IntegerSet class

--- Lab-2/Lab-2/Program.cs
+++ Lab-2/Lab-2/Program.cs
@@ -1,6 +1,40 @@
 ﻿using System;
 
 namespace Lab_2 {
+
+    public class IntegerSet {
+
+        private const int SETSIZE = 101;
+        private bool[] set;
+
+        // parameterless constructor, creates an empty set
+        public IntegerSet() {
+            set = new bool[SETSIZE];
+        } // end parameterless constructor
+
+        // constructor creates a set from array of integers
+        public IntegerSet(int[] array)
+        : this() {
+            for (int i = 0; i < array.Length; i++)
+                InsertElement(array[i]);
+        } // end constructor
+
+        public void InsertElement(int insertInteger) {
+            if (ValidEntry(insertInteger))
+                set[insertInteger] = true;
+        } // end method InsertElement
+
+        public IntegerSet Intersection(IntegerSet integerSet) {
+            IntegerSet temp = new IntegerSet();
+
+            for (int count = 0; count < SETSIZE; count++)
+                temp.set[count] = (set[count] && integerSet.set[count]);
+
+            return temp;
+        } // end method Intersection
+
+    }
+
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
